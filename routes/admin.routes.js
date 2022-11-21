39395567d8e9b1f27cba89c29fd45b1968f2aa50
umairshamoon <umairshamoon513@gmail.com>
2022-11-21 20:39:03 +0500

    woked on edits

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -1,9 +1,18 @@
 const express = require('express')
 const router = express.Router()
 const admin = require('../controllers/admin.controller')
-
+const user = require('../controllers/user.controller')
+const upload = require('../middleware/multer.middleware')
 const { isLogin, isAdmin } = require('../middleware')
 
 router.post('/login', admin.login)
+router.put('/edit/profile', isLogin, isAdmin, user.editProfile)
+router.put(
+  '/edit/profile-pic',
+  isLogin,
+  isAdmin,
+  upload.single('image'),
+  user.editProfilePic
+)
 
 module.exports = router
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,6 +5,13 @@ const upload = require('../middleware/multer.middleware')
 const { isLogin, isAdmin } = require('../middleware')
 
 router.post('/login', user.login)
+router.put('/edit/profile', isLogin, user.editProfile)
+router.put(
+  '/edit/profile-pic',
+  upload.single('image'),
+  isLogin,
+  user.editProfilePic
+)
 router.post('/register', upload.single('image'), user.register)
 
 module.exports = router
