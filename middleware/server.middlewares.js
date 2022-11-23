d246c8e20b1086c1f38779998b949fde3b643d23
umairshamoon <umairshamoon513@gmail.com>
2022-11-24 02:05:33 +0500

    configs

--- middleware/server.middlewares.js
+++ middleware/server.middlewares.js
@@ -1,5 +1,7 @@
 const cors = require('cors')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const compression = require('compression')
 const bodyParser = require('body-parser')
 
 module.exports = function (app, express) {
@@ -7,5 +9,7 @@ module.exports = function (app, express) {
   app.use(bodyParser.urlencoded({ extended: true }))
   app.use(bodyParser.json())
   app.use(cors())
+  app.use(helmet())
+  app.use(compression())
   app.use(morgan('dev'))
 }
