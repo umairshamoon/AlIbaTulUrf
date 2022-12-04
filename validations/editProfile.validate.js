03476640f5243bed556bf8aa2f8db97731649665
umairshamoon <umairshamoon513@gmail.com>
2022-12-04 18:25:04 +0500

    email,password edit

--- validations/editProfile.validate.js
+++ validations/editProfile.validate.js
@@ -3,6 +3,8 @@ module.exports = function (user) {
   const userSchema = Joi.object({
     username: Joi.string().min(3).max(50).required(),
     contact: Joi.string().length(11).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(8).required(),
   })
   return userSchema.validate(user)
 }
